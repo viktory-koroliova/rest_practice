41f52c2bb281f05a7f7b64569b5fb3928dbf52a6
Vika <viktorykoroliova@gmail.com>
2023-03-22 19:29:47 +0200

    added Book model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD"
+        SOFT = "SOFT"
+
+    title = models.CharField(max_length=255, unique=True)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=50, choices=CoverChoices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.title
